540ebe7fb7ee4dc9821997b4fc4e3d0b3c956b23
Pavan576 <49777725+Pavan576@users.noreply.github.com>
2019-12-30 23:33:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
-exports.TOKEN = 'TOKEN'; //THIS BOT IS FOR MULTIPLE GUILD
-exports.PREFIX = 'PREFIX';
+exports.TOKEN = 'NjM1Mzg1NjIyNjQ2ODgyMzE0.Xgo3LQ.TtZVm-hzdMeJujXuNR5vFs5yzRU'; //THIS BOT IS FOR MULTIPLE GUILD
+exports.PREFIX = 'm!';
 
 exports.GOOGLE_API_KEY = 'AIzaSyC8GHxFJ17uRpUYuyUngZ4lrlWNV_P85-w';
 exports.COLOR = '#fffff'; //COLOR OF EMBED
-exports.STATUS = `YOUR STATUS`; //ADD YOUR BOT STATUS
+exports.STATUS = `By MUNNA `; //ADD YOUR BOT STATUS
